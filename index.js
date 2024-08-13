310c3724c9170b89f9ca773cb84cd2fdb9b984a8
jesustellor <22569672+jesustellor@users.noreply.github.com>
2024-08-12 17:22:03 -0700

    starting the trading card game

---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require('express')();
+const PORT = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const app = require('express')();
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
